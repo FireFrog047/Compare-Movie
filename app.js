062171288d8d04865375db55636b332ae9e5b9fc
FireFrog <monjurul.47@gmail.com>
2020-08-12 01:29:23 +0600

    under construct

--- app.js
+++ app.js
@@ -1,58 +1,111 @@
-const fetchData = async (searchTerm) => {
+const leftSummary = document.querySelector('#left-summary');
+const rightSummary = document.querySelector('#right-summary');
+
+const autoCompleteConfig = {
+  async fetchData(searchTerm) {
     const response = await axios.get('http://www.omdbapi.com/', {
-        params: {
-            apikey: '556d73c4',
-            s: searchTerm
-        }
+      params: {
+        apikey: '556d73c4',
+        s: searchTerm
+      }
     })
     if (response.data.Error) {
-        return [];
+      return [];
     }
     return response.data.Search;
+
+  },
+
+  renderOption(movie) {
+    const imgSrc = movie.Poster === 'N/A' ? 'no-image-placeholder.jpg' : movie.Poster;
+    return `
+      <img src="${imgSrc}"/>
+      <span>${movie.Title}</span>
+      <span>&nbsp(${movie.Year})</span>
+    `
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
 };
 
-const input = document.querySelector('.input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWraper = document.querySelector('.results');
+autoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#left-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.showMessage').classList.add('is-hidden');
+    movieOnSelect(movie, leftSummary);
+  },
+});
+autoComplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(movie) {
+    document.querySelector('.showMessage').classList.add('is-hidden');
+    movieOnSelect(movie, rightSummary);
+  },
+});
 
-const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
+const movieOnSelect = async (movie, summary) => {
+  if (movie.imdbID) {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: '556d73c4',
+        i: movie.imdbID,
+      }
+    })
+    summary.innerHTML = movieTemplate(response.data);
+  } else {
+    summary('#summary').innerHTML = ``;
+  }
 
-    if (movies.length == 0) {
-        movies[0] = {
-            Poster: 'N/A',
-            Title: `No movies found with "${input.value.bold()}"`
-        }
-    }
-
-    resultsWraper.innerHTML = '';
-    dropdown.classList.add('is-active');
-
-    for (let movie of movies) {
-        const option = document.createElement('a');
-        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-        option.classList.add('dropdown-item');
-        option.innerHTML = `
-        <img src="${imgSrc}"/>
-        ${movie.Title}
-        `;
-        resultsWraper.appendChild(option);
-    }
-    if (input.value.length == 0) {
-        dropdown.classList.remove('is-active');
-    }
 };
 
-input.addEventListener('input', debounce(onInput));
-const root = document.querySelector('.autocomplete');
 
-document.addEventListener('click', (event) => {
-    if (input.value.length == 0) {
-        dropdown.classList.remove('is-active');
-    } else if (!root.contains(event.target)) {
-        dropdown.classList.remove('is-active');
-    } else {
-        dropdown.classList.add('is-active');
-    }
-})
+const movieTemplate = movieDetail => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${movieDetail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title} (${movieDetail.Year})</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary awardSize">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article id="imdb" class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
+};
+
+const compare = () => {
+  let rating = leftSummary.querySelector('#imdb').querySelector('title');
+  console.log(rating);
+};
+
+
+parseInt(leftSummary.querySelector('#imdb').querySelector('.title').innerText.replace(/,/g, ''));
+leftSummary.querySelector('#imdb').querySelector('.title').innerText
--- utility.js
+++ utility.js
@@ -9,3 +9,80 @@ const debounce = (func, delay = 1000) => {
         }, delay)
     }
 };
+
+const autoComplete = ({
+    fetchData,
+    root,
+    renderOption,
+    onOptionSelect,
+    inputValue,
+}) => {
+    root.innerHTML = `
+        <label><b>Search</b></label>
+        <input class = "input" />
+         <div class = "dropdown">
+             <div class = "dropdown-menu" >
+                <div class = "dropdown-content results" > </div>
+            </div> 
+        </div>           
+`;
+
+    const input = root.querySelector('.input');
+    const dropdown = root.querySelector('.dropdown');
+    const resultsWraper = root.querySelector('.results');
+
+    const onInput = async (event) => {
+        let items = await fetchData(event.target.value);
+
+        resultsWraper.innerHTML = '';
+        dropdown.classList.add('is-active');
+
+        if (items.length == 0) {
+            const option = document.createElement('a');
+            option.classList.add('dropdown-item');
+            option.innerHTML = `<span>Nothing found with "${event.target.value.bold()}"</span>`;
+            resultsWraper.appendChild(option);
+            option.addEventListener('click', async () => {
+                onOptionSelect(items[0] = {
+                    searchStatus: 'Nothing found',
+                });
+                dropdown.classList.remove('is-active');
+            });
+        } else {
+            for (let item of items) {
+                const option = document.createElement('a');
+
+                option.classList.add('dropdown-item');
+                option.innerHTML = renderOption(item);
+                resultsWraper.appendChild(option);
+
+                option.addEventListener('click', async () => {
+                    input.value = inputValue(item);
+                    onOptionSelect(item);
+                    dropdown.classList.remove('is-active');
+                });
+            };
+        }
+
+        if (input.value.length == 0) {
+            dropdown.classList.remove('is-active');
+        };
+    };
+
+    input.addEventListener('input', debounce(onInput));
+
+    document.addEventListener('click', (event) => {
+        if (!root.contains(event.target)) {
+            dropdown.classList.remove('is-active');
+        }
+    });
+
+    input.addEventListener('click', () => {
+
+        if (input.value.length !== 0) {
+            dropdown.classList.add('is-active');
+        } else {
+            dropdown.classList.remove('is-active');
+        }
+    });
+};
